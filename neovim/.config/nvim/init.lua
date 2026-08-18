3f81343851a82e4b9a2d17872a9b0afc3f5147ea
Michael Luu <67599521+kemonprogrammer@users.noreply.github.com>
2026-08-19 00:59:50 +0200

    toggle NERDTree with find file

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -209,8 +209,18 @@ vim.api.nvim_set_keymap('v', '<C-s>', '<cmd>update<CR>', { desc = 'Save' })
 vim.api.nvim_set_keymap('i', '<C-s>', '<C-o><cmd>update<CR>', { desc = 'Save and return to insert mode' })
 
 ---- Leader shortcuts
-vim.keymap.set('n', '<leader>e', '<cmd>NERDTreeToggle<CR>', { desc = 'Toggle Nerdtree' })
---nnoremap <leader>et :NERDTreeToggle<CR>
+vim.keymap.set('n', '<leader>e', function()
+  -- Evaluate the Vimscript dictionary method directly
+  local is_open = vim.api.nvim_eval('g:NERDTree.IsOpen()') == 1
+
+  if is_open then
+    vim.cmd('NERDTreeToggle')
+  else
+    vim.cmd('NERDTreeFind')
+  end
+end, { desc = 'Find or Close NERDTree' })
+-- vim.keymap.set('n', '<leader>er', '<cmd>NERDTreeFind<CR>', { desc = 'Toggle Nerdtree' })
+
 ---- nmap <Leader>fzf :Files<CR>
 --nmap <Leader>f :GFiles<CR>
 
